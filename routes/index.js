6445dbfc284ca95279849fa642f092bfc4514af1
asshevchenko <asshevchenko@domclick.ru>
2017-11-28 17:10:19 +0300

    Collection processing; handling parametrized routes

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,22 @@ var router = express.Router();
 var _ = require('lodash')
 
 var fs = require('fs'), // needed to read JSON file from disk
-  Collection = require('postman-collection').Collection,
-  myCollection;
+    Collection = require('postman-collection').Collection,
+    myCollection;
 
 myCollection = new Collection(JSON.parse(fs.readFileSync('sample.postman_collection.json').toString()));
-const members = _.flatten(myCollection.items.members.map(item => item.items))
-const requestResponseList = _.flatten(members.map(i => i.members)).filter(listitem => listitem.responses.members.length)
+const members = myCollection.items.members
 
-requestResponseList.forEach(item => {
-  const route = `/${item.request.url.path.join('/')}`
-  router[item.request.method.toLowerCase()](route, function (req, res, next) {
-    res.header('Content-Type', 'application/json')
-    res.send(item.responses.members[0].body)
-  });
+members.forEach(item => {
+
+    const route = `/${item.request.url.path.map(item => item.startsWith('{{')
+        ? item.replace('{{', ':').replace('}}', '')
+        : item).join('/')}`
+
+    router[item.request.method.toLowerCase()](route, function (req, res, next) {
+        res.header('Content-Type', 'application/json')
+        res.send(item.responses.members[0].body)
+    });
 })
 
 module.exports = router
